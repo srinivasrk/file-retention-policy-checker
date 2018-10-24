f35aca90c722b5d37e3580409ad1df9f626b68a6
srinivasrk <srinivasrk92@gmail.com>
2018-10-24 11:28:14 -0400

    promisify the readDir and use readJSONSync

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-const fs = require('fs-extra');
+const fs = require('fs-extra')
 const tar = require('tar')
 const path = require( 'path' );
 const _ = require('underscore')
+const moment = require('moment')
+
 let sourcePath = process.env.FRP_PATH // a path to search for manifest
 let rp = process.env.FRP_RP // a integer for retention policy. EG: 30 -> remove all files older than 30 days
 
 // go to the path and find manifest.json and check if the date is execeeding the retention policy. If so, add it to tar and then delete those files.
+let now = moment().utc()
+let toZip = []
+let counter = 0
 
-fs.readdir(sourcePath, (err, folder) => {
-  _.each(folder, (folderName) => {
-    let manifest = path.resolve(sourcePath, folderName, 'manifest.json')
-    fs.readJson(manifest).then((fileDetails) => {
-      let date = fileDetails.date
-      // check if date has exceeded retention policy. If so schedule it for tar.
-
-    }).catch((err) => {
-      console.log(err);
+async function readDirectoryAsync(sourcePath) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(sourcePath, (err, folder) => {
+      _.each(folder, (folderName) => {
+        counter += 1
+        let manifest = path.resolve(sourcePath, folderName, 'manifest.json')
+        let manifestObj = fs.readJsonSync(manifest, { throws: false })
+        if(manifestObj) {
+          let date = moment(manifestObj.date)
+            // check if date has exceeded retention policy. If so schedule it for tar.
+          let diff_date = (Math.abs(date.diff(now, 'days'))) // is the number of days its older by
+          if(diff_date >= rp) {
+            toZip.push(path.resolve(sourcePath, folderName))
+            if(counter == folder.length) {
+              resolve(toZip)
+            }
+          }
+        }
+      })
     })
   })
+}
 
+readDirectoryAsync(sourcePath).then((toZip) => {
+  console.log(toZip);
 })
+
+
 
 
 // tar.c(
