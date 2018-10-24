c2d3cd784948a76f38b6db8e9dfc6dc1e56b616c
srinivasrk <srinivasrk92@gmail.com>
2018-10-24 13:15:39 -0400

    fix variable scopes

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ const moment = require('moment')
 
 let sourcePath = process.env.FRP_PATH // a path to search for manifest
 let rp = process.env.FRP_RP // a integer for retention policy. EG: 30 -> remove all files older than 30 days
-
-// go to the path and find manifest.json and check if the date is execeeding the retention policy. If so, add it to tar and then delete those files.
 let now = moment().utc()
-let toZip = []
-let counter = 0
 
-async function readDirectoryAsync(sourcePath) {
+function readDirectoryAsync(sourcePath) {
+  let toZip = []
+  let counter = 0
   return new Promise((resolve, reject) => {
+    // go to the path and find manifest.json and check if the date is execeeding the retention policy. If so, add it to tar and then delete those files.
     fs.readdir(sourcePath, (err, folder) => {
       _.each(folder, (folderName) => {
         counter += 1
@@ -21,7 +20,7 @@ async function readDirectoryAsync(sourcePath) {
         let manifestObj = fs.readJsonSync(manifest, { throws: false })
         if(manifestObj) {
           let date = moment(manifestObj.date)
-            // check if date has exceeded retention policy. If so schedule it for tar.
+          // check if date has exceeded retention policy. If so schedule it for tar.
           let diff_date = (Math.abs(date.diff(now, 'days'))) // is the number of days its older by
           if(diff_date >= rp) {
             toZip.push(path.resolve(sourcePath, folderName))
